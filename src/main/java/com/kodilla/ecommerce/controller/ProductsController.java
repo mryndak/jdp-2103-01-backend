0c16db4381b70cd6202284d870fa62df01ec9b67
R4K3 <73075897+CodingWithR4K3@users.noreply.github.com>
2021-03-30 19:39:06 +0200

    JDP210303-1 - ProductController

--- src/main/java/com/kodilla/ecommerce/controller/ProductsController.java
+++ src/main/java/com/kodilla/ecommerce/controller/ProductsController.java
@@ -1,16 +1,16 @@
 package com.kodilla.ecommerce.controller;
 
 import com.kodilla.ecommerce.dto.ProductDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
-@RequestMapping("/v1/products")
+@RequestMapping(value = "/v1/products",consumes = APPLICATION_JSON_VALUE)
 public class ProductsController {
 
     @GetMapping
@@ -29,5 +29,31 @@ public class ProductsController {
         );
     }
 
-    // TODO: Pozostałe metody z zadania
+    @GetMapping("/{id}")
+    public ProductDto getProduct(@PathVariable Long id ) {
+        return ProductDto.builder()
+                .id(1L)
+                .name("kurtka zimowa")
+                .build();
+    }
+
+    @PostMapping
+    public ProductDto createProduct(@RequestBody final ProductDto productDto) {
+        return ProductDto.builder()
+                .id(1L)
+                .name("kurtka zimowa - created")
+                .build();
+    }
+
+    @PutMapping
+    public ProductDto updateProduct(@RequestBody final ProductDto productDto) {
+        return ProductDto.builder()
+                .id(1L)
+                .name("kurtka zimowa - updated")
+                .build();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable final Long id) {
+    }
 }
