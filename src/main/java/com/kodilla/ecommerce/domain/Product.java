ec8db8e67f406c0b0947e320f3ae5477ab0a5531
CodingWithR4K3 <Gorgol.Konrad@protonmail.com>
2021-03-30 20:13:31 +0200

    JDP210303-12 - Encja Product

---
+++ src/main/java/com/kodilla/ecommerce/domain/Product.java
@@ -0,0 +1,36 @@
+package com.kodilla.ecommerce.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "group_id")
+    private Long groupId;
+
+}
+
