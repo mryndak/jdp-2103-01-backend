2ad5822fb7fd9335bbdaffdd9bdd1dcb967192c9
sitkositkowski <sitkositkowski@wp.pl>
2021-04-01 00:13:36 +0200

    JDP210303-5 - UserController

--- src/main/java/com/kodilla/ecommerce/controller/UsersController.java
+++ src/main/java/com/kodilla/ecommerce/controller/UsersController.java
@@ -13,22 +13,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UsersController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getUsers")
+    @GetMapping
     public List<UserDto> getUsers() {
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getUser")
-    public UserDto getUser(@RequestParam Long userId) {
+    @GetMapping(value = "{userId}")
+    public UserDto getUser(@PathVariable Long userId) {
         return new UserDto(1L,"Test User", "1", 10000L);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "blockUser")
+    @PutMapping
     public void blockUser(@RequestBody UserDto userDto){
         userDto.setStatus("0");
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createUser")
+    @PostMapping(value = "createUser")
     public void createUser(@RequestBody UserDto userDto) {
 
     }
--- src/main/java/com/kodilla/ecommerce/dto/UserDto.java
+++ src/main/java/com/kodilla/ecommerce/dto/UserDto.java
@@ -3,9 +3,11 @@ package com.kodilla.ecommerce.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
     private Long id;
