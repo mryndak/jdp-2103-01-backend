70a0d2dd9bbd6e0847455ed201422402a6817f20
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2021-04-01 12:47:37 +0200

    JDP210303-2 - Encja Group - update

--- src/main/java/com/kodilla/ecommerce/domain/Group.java
+++ src/main/java/com/kodilla/ecommerce/domain/Group.java
@@ -1,4 +1,28 @@
 package com.kodilla.ecommerce.domain;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "groups")
 public class Group {
+
+    @NotNull
+    @Id
+    @GeneratedValue
+    private Long groupId;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
 }
