7c2f747f4b51db9c2c1bbc8b3e6d88019ca99531
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2021-04-01 10:46:00 +0200

    JDP210303-2 - GroupController

---
+++ src/main/java/com/kodilla/ecommerce/dto/GroupDto.java
@@ -0,0 +1,14 @@
+package com.kodilla.ecommerce.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class GroupDto {
+
+    private Long groupId;
+    private String name;
+
+}
