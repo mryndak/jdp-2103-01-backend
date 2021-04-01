c8f4fe54956b7b2d5ee1d5a6e09fb396ba5da642
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2021-04-01 13:05:48 +0200

    JDP210303-13 - Encja Group - update

--- src/main/java/com/kodilla/ecommerce/domain/Group.java
+++ src/main/java/com/kodilla/ecommerce/domain/Group.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,7 +23,7 @@ public class Group {
     private Long groupId;
 
     @NotNull
-    @Column(name = "name")
+    @Size(min = 4, max = 50)
     private String name;
 
 }
