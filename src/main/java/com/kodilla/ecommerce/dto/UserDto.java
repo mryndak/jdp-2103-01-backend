b0329908fe5ccf58a67d208eb9afb45d9d9eb7bf
sitkositkowski <74245529+sitkositkowski@users.noreply.github.com>
2021-04-02 21:35:49 +0200

    JDP210303-5 - UserController
    * JDP210303-5 - UserController

---
+++ src/main/java/com/kodilla/ecommerce/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.kodilla.ecommerce.dto;
+
+import com.kodilla.ecommerce.domain.enums.StatusUser;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+
+    private Long id;
+    private String username;
+    private StatusUser status;
+    private Long userKey;
+
+
+}
