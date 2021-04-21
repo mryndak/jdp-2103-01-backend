9afe039bb5816e03624073bbafa4b63da71b965c
Janusz Pasternak <janusz_pasternak@tlen.pl>
2021-04-21 05:32:38 +0200

    ProductService - save and update CORRECTION

--- src/main/java/com/kodilla/ecommerce/service/ProductService.java
+++ src/main/java/com/kodilla/ecommerce/service/ProductService.java
@@ -1,10 +1,14 @@
 package com.kodilla.ecommerce.service;
 
+import com.kodilla.ecommerce.domain.Group;
 import com.kodilla.ecommerce.domain.Product;
 import com.kodilla.ecommerce.dto.ProductDto;
 import com.kodilla.ecommerce.mapper.ProductMapper;
+import com.kodilla.ecommerce.repository.GroupRepository;
 import com.kodilla.ecommerce.repository.ProductRepository;
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +21,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final GroupRepository groupRepository;
 
     public List<ProductDto> getAllProducts() {
         return productRepository.findAll()
@@ -30,19 +35,32 @@ public class ProductService {
                 .map(productMapper::mapToProductDto);
     }
 
+    @SneakyThrows
     public ProductDto saveProduct(final ProductDto productDto) {
         Product productToSave = productMapper.mapToProduct(productDto);
+        Optional<Group> group = groupRepository.findById(productDto.getGroupId());
+        if (group.isPresent()) {
+            Group productGroup = group.get();
+            productToSave.setGroup(productGroup);
+        } else {
+            throw new NotFoundException("Group doesn't exist, please change the group.");
+        }
         productToSave = productRepository.save(productToSave);
         return productMapper.mapToProductDto(productToSave);
     }
 
     public Optional<ProductDto> updateProduct(final ProductDto productDto) {
         Optional<Product> product = productRepository.findById(productDto.getId());
-        if(product.isPresent()) {
+        if (product.isPresent()) {
             product.ifPresent(p -> {
                 p.setName(productDto.getName());
                 p.setDescription(productDto.getDescription());
                 p.setPrice(productDto.getPrice());
+                Optional<Group> group = groupRepository.findById(productDto.getGroupId());
+                if (group.isPresent()) {
+                    Group productGroup = group.get();
+                    p.setGroup(productGroup);
+                }
             });
             productRepository.save(product.get());
             return product.map(productMapper::mapToProductDto);
