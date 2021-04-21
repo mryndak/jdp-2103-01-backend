5f0e54b314faa4821dc2209148c6aaa42b0a0ed7
Mateusz Ryndak <kontakt@mryndak.pl>
2021-04-21 19:22:12 +0200

    Update ProductsController.java

--- src/main/java/com/kodilla/ecommerce/controller/ProductsController.java
+++ src/main/java/com/kodilla/ecommerce/controller/ProductsController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping(value = "/v1/products", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/v1/products")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class ProductsController {
