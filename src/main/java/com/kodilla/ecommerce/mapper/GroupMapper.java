8fbc676491422f1a9a222e9bacd3cc62760e1642
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2021-04-21 11:38:59 +0200

    JDP210303-22

--- src/main/java/com/kodilla/ecommerce/mapper/GroupMapper.java
+++ src/main/java/com/kodilla/ecommerce/mapper/GroupMapper.java
@@ -16,12 +16,9 @@ public interface GroupMapper {
     @Mapping(target = "products", ignore = true)
     Group mapToGroup(final GroupDto groupDto);
 
-    Group mapToGroup(final GroupDto groupDto, final List<Product> products);
-
     @Mapping(target = "products", ignore = true)
     GroupDto mapToGroupDto(final Group group);
 
-    GroupDto mapToGroupDto(final Group group, final List<ProductDto> products);
 
     List<GroupDto> mapToGroupDtoList(final List<Group> groupList);
 
