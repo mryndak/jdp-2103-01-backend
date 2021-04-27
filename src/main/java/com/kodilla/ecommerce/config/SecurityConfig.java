8f9a80e3be968f3a89163bb706b23f543c58a5d7
sitkositkowski <sitkositkowski@wp.pl>
2021-04-27 13:42:28 +0200

    31.2 Added Spring Security with JSON authorization

--- src/main/java/com/kodilla/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/kodilla/ecommerce/config/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.kodilla.ecommerce.config;
 
+import com.kodilla.ecommerce.config.security.JsonObjectAuthenticationFilter;
+import com.kodilla.ecommerce.config.security.RestAuthenticationFailureHandler;
+import com.kodilla.ecommerce.config.security.RestAuthenticationSuccessHandler;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,10 +14,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = false)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private RestAuthenticationSuccessHandler authenticationSuccessHandler;
+    private RestAuthenticationFailureHandler authenticationFailureHandler;
+
+    public SecurityConfig(RestAuthenticationSuccessHandler authenticationSuccessHandler,
+                          RestAuthenticationFailureHandler authenticationFailureHandler) {
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+        this.authenticationFailureHandler = authenticationFailureHandler;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder builder) throws Exception {
@@ -19,6 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("{noop}password")
                 .roles("USER");
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -27,9 +46,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().permitAll();
+                .addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class); // 1
+                //.formLogin().permitAll();
                 //.and()
                 //.exceptionHandling() // 1
                 //.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)); //
     }
+
+
+    @Bean
+    public JsonObjectAuthenticationFilter authenticationFilter() throws Exception {
+        JsonObjectAuthenticationFilter filter = new JsonObjectAuthenticationFilter();
+        filter.setAuthenticationSuccessHandler(authenticationSuccessHandler);
+        filter.setAuthenticationFailureHandler(authenticationFailureHandler);
+        filter.setAuthenticationManager(super.authenticationManager());
+        return filter;
+    }
+
 }
